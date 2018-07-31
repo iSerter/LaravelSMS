4fb1885fa855d15d425c3715c102470ee7c778d7
ilyasserter <ilyasserter@teknasyon.com>
2018-07-31 19:33:30 +0300

    update composer json to require laravel 5.4 components and service provider to use corressponding singleton method instead of share

--- src/SimpleSoftwareIO/SMS/SMSServiceProvider.php
+++ src/SimpleSoftwareIO/SMS/SMSServiceProvider.php
@@ -64,7 +64,7 @@ class SMSServiceProvider extends ServiceProvider
      */
     public function registerSender()
     {
-        $this->app['sms.sender'] = $this->app->share(function ($app) {
+        $this->app['sms.sender'] = $this->app->singleton(function ($app) {
             return (new DriverManager($app))->driver();
         });
     }
